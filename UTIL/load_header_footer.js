b0b8c5220bea803091589855eb928dab898806d8
ruxi <ruxi.m13@gmail.com>
2021-01-20 18:44:32 +0000

    header & footer

---
+++ UTIL/load_header_footer.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $('header').load("../UTIL/pheader.html",function() {
+
+        $('.hero-headline').text($('title').text());
+
+        $('.hamburger-menu').on('click', function () {
+            console.log('hambutger clicked');
+            $('.toggle').toggleClass('open');
+            $('.nav-list').toggleClass('open');
+        });
+    });
+
+    $('footer').load("../UTIL/pfooter.html");
+
+});
+
