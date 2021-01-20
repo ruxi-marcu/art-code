d07a802c717010f8c1e67f7c23b27ceeaa62bdfe
ruxi-marcu <77745316+ruxi-marcu@users.noreply.github.com>
2021-01-20 17:25:00 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+$(function () { /*document ready function*/
+    
+    $('.hamburger-menu').on('click', function () {
+        $('.toggle').toggleClass('open');
+        $('.nav-list').toggleClass('open');
+    });
+
+    // $('.proj').on('click',function() {
+
+    // });
+
+    
+});
+
+
+
+// let body = document.getElementsByClassName('body');
+// console.log('sth');
+// console.log(body);
