6921bf54325995c4adf1c9b17542dfc3ff085f6e
ruxi <ruxi.m13@gmail.com>
2021-01-21 01:35:29 +0000

    formspree

--- contact.js
+++
@@ -1,6 +0,0 @@
-$('#send').on('click', function(e) {
-  e.preventDefault();
-  subject = $('#subject').val();
-  body = $('#body').val();
-  window.location = "mailto:ruxi.marcu@gmail.com.com?subject=" + subject + "&body=" + body;
-});
