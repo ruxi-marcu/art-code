d61bbc9b43e22ed27bd90f0608b40a49394f9e03
ruxi <ruxi.m13@gmail.com>
2021-03-22 16:44:04 +0000

    fixed menu

--- UTIL/load_header_footer.js
+++ UTIL/load_header_footer.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         $('.hero-headline').text($('title').text());
 
         $('.hamburger-menu').on('click', function () {
-            console.log('hambutger clicked');
+            // console.log('hambutger clicked');
             $('.toggle').toggleClass('open');
             $('.nav-list').toggleClass('open');
         });
